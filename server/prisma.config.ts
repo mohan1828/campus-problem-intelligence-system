11b62f74601d15679f472f2b486de99b0ade03b2
Mohana Karupayya <mohanakarupayya016@gmail.com>
2026-06-24 15:23:08 +0530

    Fix typescript error in prisma.config.ts

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  seed: "ts-node prisma/seed.ts",
   migrations: {
     path: "prisma/migrations",
   },
